ada979ebd69b09465db7f56973c2062ba74bacbc
= <=>
2018-05-31 11:39:16 -0700

    Progressed article page

--- metrospect-web/src/App.jsx
+++ metrospect-web/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import LandingNavbar from "./components/LandingNavbar.jsx";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 import ArticlePage from "./pages/ArticlePage/ArticlePage.jsx";
+import TestingApp from "./pages/Test/ride_pricing.jsx"
 // import ResultsPage from "./pages/ResultsPage/ResultsPage.jsx";
 
 class App extends React.Component {
@@ -17,6 +18,7 @@ class App extends React.Component {
         <div className="app">
           <Route path="/articles" component={ArticlePage} />
           <Route exact path="/" component={LandingPage} />
+          <Route path="/uber" component={TestingApp} />
           {/*<Route exact path="/eat" render={() => <ResultsPage type="eat" />} />*/}
         </div>
       </BrowserRouter>
