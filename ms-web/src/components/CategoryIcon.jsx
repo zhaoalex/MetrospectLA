555eb56dddad81d13f5990ae3aa3edc74516637f
Alex Zhao <alexzhao21@gmail.com>
2018-06-02 15:40:27 -0700

    Move category icons to src/ folder so they can be minified by webpack

--- ms-web/src/components/CategoryIcon.jsx
+++ ms-web/src/components/CategoryIcon.jsx
@@ -3,7 +3,7 @@ import "styles/CategoryIcon.css";
 
 function CategoryIcon(props) {
   const category = props.category ? props.category : 'other';
-  return <img className={[props.className, "category-icon"].join(' ')} src={require(`../../public/categories/${category.toLowerCase()}.jpg`)} alt={category} />
+  return <img className={[props.className, "category-icon"].join(' ')} src={require(`public/categories/${category.toLowerCase()}.jpg`)} alt={category} />
 }
 
 export default CategoryIcon;
