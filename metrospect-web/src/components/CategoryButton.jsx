9404fd7498fa5fa2dbff1b21bdd1515127674b78
Alex Zhao <alexzhao21@gmail.com>
2018-06-02 02:28:19 -0700

    Refactor icon (and add a fallback); css fixes for last commit

--- metrospect-web/src/components/CategoryButton.jsx
+++ metrospect-web/src/components/CategoryButton.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
+import CategoryIcon from './CategoryIcon.jsx';
 import "../styles/CategoryButton.css";
 
-class CategoryButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  getImage = category => <img src={require(`../../public/categories/${category.toLowerCase()}.jpg`)} alt={category} />
-
-  render() {
-    return (
-      <button className="category-button" onClick={this.props.onClick}>
-        {this.getImage(this.props.category)}
-        <br />
-        {this.props.category}
-      </button>
-    )
-  }
+function CategoryButton(props) {
+  return (
+    <button className="category-button" onClick={props.onClick}>
+      <CategoryIcon category={props.category} className="category-button-img" />
+      <br />
+      {props.category}
+    </button>
+  )
 }
 
 export default CategoryButton;
