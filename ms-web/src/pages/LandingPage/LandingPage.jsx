cb67d53e0be3553b822571b658e47e38cb957886
Alex Zhao <alexzhao21@gmail.com>
2018-06-03 07:44:28 -0700

    Revamp landing page

--- ms-web/src/pages/LandingPage/LandingPage.jsx
+++ ms-web/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import CategoryButton from 'components/CategoryButton.jsx';
 import SearchBar from 'components/SearchBar.jsx';
 import ArticleCard from 'components/ArticleCard.jsx';
@@ -37,20 +37,41 @@ class LandingPage extends React.Component {
           </div>
         </header>
         <div id="featured">
-          <ArticleCard
-            title="Tacos at Grand Central"
-            desc="Check out one of downtown Los Angeles’ biggest attractions, and the home of many foodstagrams."
-            category="eat"
-            img="src/pages/ArticlePage/images/tacos.png"
-            showCategory
-          />
-          <ArticleCard
-            title="Tacos at Grand Central"
-            desc="Check out one of downtown Los Angeles’ biggest attractions, and the home of many foodstagrams."
-            category="eat"
-            img="src/pages/ArticlePage/images/tacos.png"
-            showCategory
-          />
+          <div className="featured-contents">
+            <h1>FEATURED</h1>
+            <div className="featured-contents-cards">
+              <ArticleCard
+                title="Tacos at Grand Central"
+                desc="Check out one of downtown Los Angeles’ biggest attractions, and the home of many foodstagrams."
+                category="eat"
+                img="src/pages/ArticlePage/images/tacos.png"
+                showCategory
+              />
+              <ArticleCard
+                title="Tacos at Grand Central"
+                desc="Check out one of downtown Los Angeles’ biggest attractions, and the home of many foodstagrams."
+                category="eat"
+                img="src/pages/ArticlePage/images/tacos.png"
+                showCategory
+              />
+            </div>
+            <Link to="/search/">Show more</Link>
+          </div>
+
+        </div>
+        <div id="about-us">
+          <div className="about-us-text">
+            <h1 style={{ color: "#FF6745" }}>ABOUT US</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi enim velit, aliquam ac posuere ac, pulvinar quis elit. Nam viverra dolor vitae nibh imperdiet venenatis. Etiam at posuere nisi. Sed condimentum nulla at rutrum faucibus. Pellentesque id nibh dui.</p>
+          </div>
+          <div className="about-us-pics">
+            <p>Pic</p>
+          </div>
+        </div>
+        <div id="footer">
+          <div className="footer-contents">
+            <h4>Made with ❤️ from the Metrospect team.</h4>
+          </div>
         </div>
         {this.state.fireRedirect && <Redirect to={`/search/${this.state.searchInput}`} />}
       </div>
